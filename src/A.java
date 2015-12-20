d4576da90088d042ae179e820249c490608c54ac
Luoh Ren-Shan (LCamel) <lcamel@yahoo-inc.com>
2015-12-20 15:19:24 +0800

    add parseOne()

--- src/A.java
+++ src/A.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,11 +8,30 @@ public class A {
     public static void main(String[] args) {
         List<String> tokens = getTokens("(+ (* 2 3) 4)");
         System.out.println("tokens: " + tokens);
-        tokens.remove(0);
+        Object expr = parseOne(tokens);
+        System.out.println("expr: " + expr);
+    }
+
+    private static Object parseOne(List<String> tokens) {
+        String token = tokens.remove(0); // "(", ")", and others
+        if (token.equals("(")) {
+            List<Object> tmp = new ArrayList<>();
+            while (true) {
+                if (tokens.get(0).equals(")")) {
+                    tokens.remove(0);
+                    return tmp;
+                }
+                tmp.add(parseOne(tokens));
+            }
+        } else if (token.equals(")")) {
+            throw new RuntimeException("found ')' but no '('");
+        } else {
+            return token;
+        }
     }
 
     private static List<String> getTokens(String line) {
         String[] tokens = line.replace("(", " ( ").replace(")", " ) ").trim().split("\\s+");
-        return new LinkedList<String>(Arrays.asList(tokens));
+        return new LinkedList<String>(Arrays.asList(tokens)); // for remove()
     }
 }
